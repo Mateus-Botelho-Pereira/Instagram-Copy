63120ee1dfc3697b1718ffab6bb116de739e7ce3
Mateus B. Pereira <mateus.botelho.pereira@gmail.com>
2022-02-03 22:28:56 -0300

    Building header

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    Image
+} from 'react-native'
+import instagram from '../../assets/images/instagram.png'
+
+class Header extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.rowContainer}>
+                    <Image source={instagram} style={styles.image} />
+                    <Text style={styles.title}>Instagram Copy</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 0,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain',
+    },
+    title: {
+        color: '#000',
+        fontFamily: 'shelter',
+        fontSize: 24,
+        height: 30
+    }
+})
+
+export default Header

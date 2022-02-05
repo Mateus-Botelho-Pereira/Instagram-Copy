cb9ed37ba06eb3786af940cb82f5b35109319a6f
Mateus B. Pereira <mateus.botelho.pereira@gmail.com>
2022-02-05 03:18:37 -0300

    Navigation Tabs

---
+++ src/Navigator.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import Feed from './screens/Feed'
+
+const Tab = createBottomTabNavigator()
+
+export default props => {
+    return (
+    <NavigationContainer>
+        <Tab.Navigator
+            initialRouteName='Feed'
+            screenOptions={{
+                tabBarShowLabel: false,
+                //tabBarActiveTintColor: '#008',
+                //tabBarInactiveTintColor: '#008000',
+            }}
+        >
+            <Tab.Screen
+                name='AddPhoto' 
+                component={Feed}
+                options={() => ({
+                    tabBarIcon: ({ color }) =>
+                        <Icon name='camera' size={30} color={color} />
+                })}           
+            />
+            <Tab.Screen
+                name='Feed' 
+                component={Feed} 
+                options={() => ({
+                    tabBarIcon: ({ color }) =>
+                        <Icon name='home' size={30} color={color} />
+                })}           
+            />
+            <Tab.Screen
+                name='Profile' 
+                component={Feed} 
+                options={() => ({
+                    tabBarIcon: ({ color }) =>
+                        <Icon name='user' size={30} color={color} />
+                })}           
+            />
+        </Tab.Navigator>
+    </NavigationContainer>
+    )
+}

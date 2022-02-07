f9f71c7d69e920b8a9ec4725fbaecceac4ad5010
Mateus B. Pereira <mateus.botelho.pereira@gmail.com>
2022-02-06 22:56:12 -0300

    Permissions

--- src/Navigator.js
+++ src/Navigator.js
@@ -9,40 +9,40 @@ const Tab = createBottomTabNavigator()
 
 export default props => {
     return (
-    <NavigationContainer>
-        <Tab.Navigator
-            initialRouteName='Feed'
-            screenOptions={{
-                tabBarShowLabel: false,
-                //tabBarActiveTintColor: '#008',
-                //tabBarInactiveTintColor: '#008000',
-            }}
-        >
-            <Tab.Screen
-                name='AddPhoto' 
-                component={Feed}
-                options={() => ({
-                    tabBarIcon: ({ color }) =>
-                        <Icon name='camera' size={30} color={color} />
-                })}           
-            />
-            <Tab.Screen
-                name='Feed' 
-                component={Feed} 
-                options={() => ({
-                    tabBarIcon: ({ color }) =>
-                        <Icon name='home' size={30} color={color} />
-                })}           
-            />
-            <Tab.Screen
-                name='Profile' 
-                component={Feed} 
-                options={() => ({
-                    tabBarIcon: ({ color }) =>
-                        <Icon name='user' size={30} color={color} />
-                })}           
-            />
-        </Tab.Navigator>
-    </NavigationContainer>
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName='Feed'
+                screenOptions={{
+                    tabBarShowLabel: false,
+                    //tabBarActiveTintColor: '#008',
+                    //tabBarInactiveTintColor: '#008000',
+                }}
+            >
+                <Tab.Screen
+                    name='AddPhoto' 
+                    component={Feed}
+                    options={() => ({
+                        tabBarIcon: ({ color }) =>
+                            <Icon name='camera' size={30} color={color} />
+                    })}           
+                />
+                <Tab.Screen
+                    name='Feed' 
+                    component={Feed} 
+                    options={() => ({
+                        tabBarIcon: ({ color }) =>
+                            <Icon name='home' size={30} color={color} />
+                    })}           
+                />
+                <Tab.Screen
+                    name='Profile' 
+                    component={Feed} 
+                    options={() => ({
+                        tabBarIcon: ({ color }) =>
+                            <Icon name='user' size={30} color={color} />
+                    })}           
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
